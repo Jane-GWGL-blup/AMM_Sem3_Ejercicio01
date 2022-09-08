86a54f4cb2a423928622ca8a10ddce1b150d2610
Nicole Caceres <janenieggcs@gmail.com>
2022-09-07 22:46:50 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio01/Ejercicio01/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ejercicio01
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new StackLayoutDemo();
+            //MainPage = new GridDemo();
+            //MainPage = new StackLayoutCode();
+            MainPage = new GridCode();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
